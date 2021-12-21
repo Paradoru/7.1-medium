af8ec035986d7baa80f73ddf22d77466bb44f27d
Paradoru <LicLexa@yandex.ru>
2021-12-21 15:43:16 +0200

    Добавьте файлы проекта.

---
+++ 7.1 средний уровень/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _7._1_средний_уровень
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int count = 0;
+                char nik = 'k';
+                Console.WriteLine("Введите текст");
+                string text = Console.ReadLine();
+                string[] words = text.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (char chacha in words[words.Length - 1])
+                {
+                    if (chacha == nik) count++;
+                }
+                Console.WriteLine("Букв k в последнем слове {0}", count);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
